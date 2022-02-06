9655bf2b14022feac663214f5be028cb3d1f0afc
yogeshwaran <yogeshwaran.ma@gmail.com>
2022-02-06 12:07:28 +0100

    last commit

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -47,4 +47,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+    @Test
+  public void greetShouldIncludeGreetingMessage2() {
+    String someone = "Hello world1, This shoud work1";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
